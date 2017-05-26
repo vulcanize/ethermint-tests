830ac0251e95a8e2d11d18d2a38faac474b60ecb
Node <nodar.chkuaselidze@gmail.com>
2017-05-26 21:30:39 +0400

    Add different tests

--- tools/diff.sh
+++ tools/diff.sh
@@ -3,6 +3,5 @@
 TMPDIR=/tmp/truffle-tests
 TESTNAME=test1
 
-
-diff <(cat $TMPDIR/$TESTNAME.output.ethereum | sed 's/(.*)//') \
+diff $@ <(cat $TMPDIR/$TESTNAME.output.ethereum | sed 's/(.*)//') \
     <(cat $TMPDIR/$TESTNAME.output.ethermint | sed 's/(.*)//')
